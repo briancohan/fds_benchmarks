0f0e0201289864163f9b7b1d6627da774a5d8e7e
briancohan <briancohan@gmail.com>
2021-02-26 10:33:45 -0500

    saved figures

--- benchmark/graph.py
+++ benchmark/graph.py
@@ -1,5 +1,6 @@
-import pandas as pd
+from pathlib import Path
 from typing import List, Dict
+import pandas as pd
 
 
 LABELS = {
@@ -14,9 +15,16 @@ LABELS = {
     'Wall time': 'Wall Time [sec]',
 }
 
+IMAGE_DIR = Path('images')
+IMAGE_DIR.mkdir(exist_ok=True)
+
 
 def category_orders(df: pd.DataFrame) -> Dict[str, List[float]]:
     return {
         col: sorted(df[col].dropna().unique())
         for col in df.loc[:, df.dtypes == 'category']
     }
+
+
+def save(fig, stem):
+    fig.write_image(str(IMAGE_DIR / f'{stem}.png'), engine='kaleido')
